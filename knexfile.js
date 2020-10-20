9db800d9bf3ea52da8075efe628d66215dc885ac
Jennifer Stewart <jstewart8053@gmail.com>
2020-10-19 19:51:40 -0500

    updates

--- knexfile.js
+++ knexfile.js
@@ -37,11 +37,13 @@ module.exports = {
     connection: pgConnection,
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
